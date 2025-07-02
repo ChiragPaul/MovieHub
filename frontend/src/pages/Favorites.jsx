04a7abab6458137b3b72f7c6337fce1e7e53eb76
Chirag Paul <chiragpaul6474@gmail.com>
2025-07-02 16:16:23 +0530

    first commit

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  const Movies = [
+    { id: 1, title: "John Wick", release_date: "2020" },
+    { id: 2, title: "Terminator", release_date: "1998" },
+    { id: 3, title: "jurassic Park", release_date: "1980" },
+    { id: 4, title: "Jack Ryan ", release_date: "2000" },
+  ];
+  return <div className="Home"></div>;
+}
