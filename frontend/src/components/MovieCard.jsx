380a5b72a6dd7e6f3cb14f33aeb69fffb4d5b467
Chirag Paul <chiragpaul6474@gmail.com>
2025-07-04 17:15:22 +0530

    Css Styling Different Pages

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,22 +1,24 @@
-
-function MovieCard({movie}) {
-        function onFavoriteClick(){
-            alert("clciked")
-        }
-    return <div className="movie-card">
-        <div className="movie-poster">
-            <img src={movie.url} alt={movie.title} />
-            <div className="movie-Overlay">
-                <button className="favorite-btn" onClick={onFavoriteClick}>
-                    (🤍)
-                </button>
-            </div>
+import "../css/MovieCard.css";
+function MovieCard({ movie }) {
+  function onFavoriteClick() {
+    alert("clciked");
+  }
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.url} alt={movie.title} />
+        <div className="movie-Overlay">
+          <button className="favorite-btn" onClick={onFavoriteClick}>
+            (🤍)
+          </button>
         </div>
-        <div className="movie-info">
-            <h3>{movie.title}</h3>
-            <p>{movie.release_date}</p>
-        </div>
+      </div>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date}</p>
+      </div>
     </div>
+  );
 }
 
-export default MovieCard
+export default MovieCard;
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import "../css/NavBar.css";
 function Navbar() {
   return (
     <nav className="navbar">
